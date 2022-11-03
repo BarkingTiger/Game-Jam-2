44c7f92386a4a4d87dc0e5d93fd41696b612e211
BarkingTiger <neodragon057@gmail.com>
2022-11-03 02:28:22 -0700

    added end interaction script and fixed note chaining

--- Assets/Scripts/EndInteraction.cs
+++ Assets/Scripts/EndInteraction.cs
@@ -2,20 +2,13 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
  
-public class PlayerInteraction : MonoBehaviour {
+public class EndInteraction : MonoBehaviour {
 
     public GameObject Player;
-    public GameObject note;
     public float minDist = 5f;
     public string text = "Spooky skeletons";
     float dist;
     bool reading = false;
-    bool spawn = false;
-    bool once = true;
-
-    void Awake() {
-        note.GetComponent<MeshRenderer>().enabled = false;
-    }
 
     void Update () {
         dist = Vector3.Distance(Player.gameObject.transform.position, gameObject.transform.position);
@@ -26,7 +19,6 @@ public class PlayerInteraction : MonoBehaviour {
                }
                else {
                    reading = true;
-                   spawn = true;
                }
          }
      }
@@ -38,11 +30,6 @@ public class PlayerInteraction : MonoBehaviour {
     void OnGUI() {
         if(reading) {
             GUI.TextArea(new Rect(Screen.height/2, Screen.width/2, 500, 500), text);
-            if (spawn && once) {
-                //note.SetActive(true);
-                note.GetComponent<MeshRenderer>().enabled = true;
-                once = false;
-            }
         }
         else if(dist <= minDist) {
             GUI.TextArea(new Rect(Screen.height/2, Screen.width/2, 500, 500), "Press 'E' to read.");
